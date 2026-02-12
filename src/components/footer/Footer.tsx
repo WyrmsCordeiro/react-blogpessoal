63944390d056221978ae0405b509810b1280ff2a
WyrmsCordeiro <wyrmsc@gmail.com>
2026-02-12 13:47:07 -0300

    estilizando o blog pessoal...

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,12 +10,18 @@ function Footer() {
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
                             Blog Pessoal Generation | Copyright: {data}
-                    </p>
+                        </p>
                 <p className='text-lg'>Acesse nossas redes sociais!</p>
                 <div className='flex gap-2'>
-                    <LinkedinLogoIcon size={48} weight='bold' />
-                    <InstagramLogoIcon size={48} weight='bold' />
-                    <FacebookLogoIcon size={48} weight='bold' />
+                    <a href="https://www.linkedin.com/in/wyrms-cordeiro-84374565567o63/" target="_blank">
+                        <LinkedinLogoIcon size={48} weight='bold' />
+                    </a>
+                    <a href="https://www.instagram.com/wyrmms" target="_blank">
+                        <InstagramLogoIcon size={48} weight='bold' />
+                    </a>  
+                    <a href="https://www.facebook.com/wyrms.cordeiro/" target="_blank">
+                        <FacebookLogoIcon size={48} weight='bold' />
+                    </a>
                 </div>    
             </div>
         </div>
