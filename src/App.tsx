b2922a22882eb7a7b8e66a8a5cff97a9c268bc7d
WyrmsCordeiro <wyrmsc@gmail.com>
2026-02-20 20:09:34 -0300

    atualizando projeto blog pessoal react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import DeletarTema from './components/tema/deletartema/DeletarTema';
+import ListaTemas from './components/tema/listatemas/ListaTemas';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import { AuthProvider } from './contexts/AuthContext';
+import FormTema from './components/tema/formtema/FormTema'
 
 function App() {
     return (
@@ -17,13 +20,17 @@ function App() {
                             <Route path="/" element={<Login />}  />
                             <Route path="/home" element={<Home />} />
                             <Route path="/cadastro" element={<Cadastro />} />
+                            <Route path="/temas" element={<ListaTemas />} />
+                            <Route path="/cadastrartema" element={<FormTema />} />
+                            <Route path="/editartema/:id" element={<FormTema />} />
+                            <Route path="/deletartema/:id" element={<DeletarTema />} />
                         </Routes>
                     </div>
                     <Footer />    
-                    </BrowserRouter>
-                    </AuthProvider>
-    </>      
-  )
+                </BrowserRouter>
+            </AuthProvider>
+        </>      
+    )
 }  
 
 export default App
